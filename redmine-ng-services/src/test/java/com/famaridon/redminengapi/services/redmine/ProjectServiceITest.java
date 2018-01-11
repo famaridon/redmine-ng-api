33547f0a30ce1d07be4e000ecade1c4fc4da679d
famaridon <famaridon@visiativ.com>
2018-01-11 23:12:08 +0100

    arquillian aware

--- redmine-ng-services/src/test/java/com/famaridon/redminengapi/services/redmine/ProjectServiceITest.java
+++ redmine-ng-services/src/test/java/com/famaridon/redminengapi/services/redmine/ProjectServiceITest.java
@@ -5,6 +5,9 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
@@ -15,19 +18,19 @@ public class ProjectServiceITest {
 		return ShrinkWrap.create(JavaArchive.class).addClass(ProjectService.class).addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 	}
 	
-	@org.junit.Before
+	@Before
 	public void setUp() throws Exception {
 	}
 	
-	@org.junit.After
+	@After
 	public void tearDown() throws Exception {
 	}
 	
-	@org.junit.Test
+	@Test
 	public void findById() {
 	}
 	
-	@org.junit.Test
+	@Test
 	public void findAll() {
 	}
 }
