3f873bbcb2ca9220330792e72bc5b1015daed242
famaridon <famaridon@visiativ.com>
2018-01-20 20:11:09 +0100

    add cache control

--- redmine-ng-web-rest-api/src/main/java/com/famaridon/redminengapi/rest/api/UsersEndpoint.java
+++ redmine-ng-web-rest-api/src/main/java/com/famaridon/redminengapi/rest/api/UsersEndpoint.java
@@ -1,5 +1,6 @@
 package com.famaridon.redminengapi.rest.api;
 
+import com.famaridon.redminengapi.rest.api.features.CacheControl;
 import com.famaridon.redminengapi.rest.dto.UserDto;
 
 import javax.ws.rs.Consumes;
@@ -8,6 +9,7 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.concurrent.TimeUnit;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -16,6 +18,7 @@ public interface UsersEndpoint {
 	
 	@GET
 	@Path("/current")
+	@CacheControl(maxAge = 15, maxAgeUnit = TimeUnit.MINUTES, isPrivate = true)
 	public UserDto findCurrent(@HeaderParam(SecurityHeaders.X_REDMINE_API_KEY) String apiKey);
 
 }
