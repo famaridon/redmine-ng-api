0db752ef3ee30fb2b7b2b2b61744ecae86ec060b
famaridon <famaridon@visiativ.com>
2018-01-23 16:52:30 +0100

    configure jackson

---
+++ redmine-ng-web-rest/src/main/java/com/famaridon/redminengapi/rest/JacksonMapperContextResolver.java
@@ -0,0 +1,26 @@
+package com.famaridon.redminengapi.rest;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class JacksonMapperContextResolver implements ContextResolver<ObjectMapper> {
+	
+	private final ObjectMapper mapper;
+	
+	public JacksonMapperContextResolver() {
+		this.mapper = new ObjectMapper();
+		this.mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		this.mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+	}
+	
+	@Override
+	public ObjectMapper getContext(Class<?> type) {
+		return this.mapper;
+	}
+	
+}
