5805ab029ba358b30527392f0b33797d6104db06
famaridon <famaridon@visiativ.com>
2018-02-04 10:58:51 +0100

    include trackers and categories on find all project

--- redmine-ng-services/src/main/java/com/famaridon/redminengapi/services/redmine/impl/DefaultProjectService.java
+++ redmine-ng-services/src/main/java/com/famaridon/redminengapi/services/redmine/impl/DefaultProjectService.java
@@ -32,7 +32,7 @@ public class DefaultProjectService extends AbstractRedmineService<Project> imple
 	
 	@Override
 	public Page<Project> findAll(String apiAccessKey, Pager pager) throws IOException {
-		Page<Project> p = Request.Get(this.configurationService.buildUrl("/projects.json?%s", pager))
+		Page<Project> p = Request.Get(this.configurationService.buildUrl("/projects.json?include=trackers,issue_categories&%s", pager))
 			.addHeader(X_REDMINE_API_KEY, apiAccessKey)
 			.execute()
 			.handleResponse(new PageResponseHandler<>(this.configurationService, Project.class));
