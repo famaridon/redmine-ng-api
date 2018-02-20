37ae5ff94536be35cba22a603b00d8ea765cb558
famaridon <famaridon@visiativ.com>
2018-02-20 11:42:48 +0100

    solve test issues

--- redmine-ng-services/src/test/java/com/famaridon/redminengapi/services/redmine/AbstractServiceITest.java
+++ redmine-ng-services/src/test/java/com/famaridon/redminengapi/services/redmine/AbstractServiceITest.java
@@ -28,6 +28,7 @@ public abstract class AbstractServiceITest {
 		
 		return ShrinkWrap.create(WebArchive.class)
 			.addPackages(true,"com.famaridon.redminengapi.services.redmine")
+			.addPackages(true,"com.famaridon.redminengapi.services.realtime")
 			.addClass(ConfigurationService.class)
 			.addClass(DefaultConfigurationService.class)
 			.addAsLibraries(dependencies)
