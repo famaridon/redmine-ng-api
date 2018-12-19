ce31156688a8011147d352619b934c1ab1f3017f
famaridon <famaridon@users.noreply.github.com>
2018-12-19 14:22:27 +0100

    use mysql not postgresql

--- docker/scripts/install-jdbc-drivers.sh
+++ docker/scripts/install-jdbc-drivers.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# Postgresql
 mkdir -p $JBOSS_HOME/modules/org/postgresql/main
 curl -L "https://jdbc.postgresql.org/download/postgresql-$POSTGRESQL_JDBC_VERSION.jar" \
     --output "$JBOSS_HOME/modules/org/postgresql/main/postgresql-$POSTGRESQL_JDBC_VERSION.jar"
@@ -11,6 +12,28 @@ cat  <<EOL > $JBOSS_HOME/modules/org/postgresql/main/module.xml
 
     <resources>
         <resource-root path="postgresql-$POSTGRESQL_JDBC_VERSION.jar"/>
+    </resources>
+
+    <dependencies>
+        <module name="javax.api"/>
+        <module name="javax.transaction.api"/>
+    </dependencies>
+</module>
+EOL
+
+# MySQL
+
+mkdir -p $JBOSS_HOME/modules/com/mysql/main
+curl -L "http://central.maven.org/maven2/mysql/mysql-connector-java/$MYSQL_JDBC_VERSION/mysql-connector-java-$MYSQL_JDBC_VERSION.jar" \
+    --output "$JBOSS_HOME/modules/com/mysql/main/mysql-connector-java-$MYSQL_JDBC_VERSION.jar"
+
+cat  <<EOL > $JBOSS_HOME/modules/com/mysql/main/module.xml
+<?xml version='1.0' encoding='UTF-8'?>
+
+<module xmlns="urn:jboss:module:1.1" name="com.mysql" slot="main">
+
+    <resources>
+        <resource-root path="mysql-connector-java-$MYSQL_JDBC_VERSION.jar"/>
     </resources>
 
     <dependencies>
