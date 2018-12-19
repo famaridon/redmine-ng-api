4ace815e6efcb8853168fe239642ebfbd0be502b
famaridon <famaridon@users.noreply.github.com>
2018-12-19 13:17:12 +0100

    add needed endpoints

--- redmine-ng-web-rest-api/src/main/java/com/famaridon/redminengapi/rest/api/IterationEndpoint.java
+++ redmine-ng-web-rest-api/src/main/java/com/famaridon/redminengapi/rest/api/IterationEndpoint.java
@@ -6,6 +6,8 @@ import com.famaridon.redminengapi.rest.dto.PageDto;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -18,4 +20,11 @@ public interface IterationEndpoint
 {
 	@GET
 	public PageDto<IterationDto> findAll(@HeaderParam(SecurityHeaders.X_REDMINE_API_KEY) String apiKey) throws IOException;
+
+	@POST
+	public IterationDto create(@HeaderParam(SecurityHeaders.X_REDMINE_API_KEY) String apiKey, IterationDto iterationDto) ;
+
+	@PUT
+	@Path("/{id}")
+	public IterationDto update(@HeaderParam(SecurityHeaders.X_REDMINE_API_KEY) String apiKey, IterationDto iterationDto) ;
 }
