cae1b58c8da069e08c871f235cad663ddcf82053
famaridon <famaridon@users.noreply.github.com>
2019-01-07 23:40:15 +0100

    wait for wildfly

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,6 +6,13 @@ cd docker
 docker-compose -f docker-compose-test.yml up --detach --force-recreate
 
 cd ..
+
+echo "Waiting wildfly to launch on 8080..."
+while ! nc -z localhost 8080; do
+  sleep 1 # wait for 1 second before check again
+done
+echo "wildfly launched"
+
 mvn -P with-test-env test
 
 cd docker
