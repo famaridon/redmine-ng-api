c1008c92ce76cb9d8195dca405487d9f20bad6fa
famaridon <famaridon@users.noreply.github.com>
2019-02-04 14:33:39 +0100

    JDK 9+ debug must specified listening interfaces

--- docker/api/scripts/start.sh
+++ docker/api/scripts/start.sh
@@ -7,4 +7,4 @@ cp -f $JBOSS_HOME/staging/* $JBOSS_HOME/standalone/deployments/
 
 $JBOSS_HOME/wait
 
-$JBOSS_HOME/bin/standalone.sh -b=0.0.0.0 -bmanagement=0.0.0.0 --debug 5000
+$JBOSS_HOME/bin/standalone.sh -b=0.0.0.0 -bmanagement=0.0.0.0 --debug *:5000
