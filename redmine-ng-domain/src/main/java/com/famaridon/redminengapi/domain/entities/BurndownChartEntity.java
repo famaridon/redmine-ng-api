cd1f77e0fac64dd284f36fb432546bad49223e08
famaridon <famaridon@users.noreply.github.com>
2019-02-07 00:03:03 +0100

    missing equals

--- redmine-ng-domain/src/main/java/com/famaridon/redminengapi/domain/entities/BurndownChartEntity.java
+++ redmine-ng-domain/src/main/java/com/famaridon/redminengapi/domain/entities/BurndownChartEntity.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 @NamedQuery(name = "BurndownChartEntity.findByIteration", query = "select b from BurndownChartEntity b where b.iteration = :iteration")
 @NamedQuery(name = "BurndownChartEntity.findCurrent", query = "select b from BurndownChartEntity b where b.iteration.start < :now and :now <= b.iteration.end")
@@ -34,5 +36,33 @@ public class BurndownChartEntity extends AbstractEntity {
   public void setValues(
       List<ChartTimedValueEntity> values) {
     this.values = values;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (!(o instanceof BurndownChartEntity)) {
+      return false;
+    }
+
+    BurndownChartEntity that = (BurndownChartEntity) o;
+
+    return new EqualsBuilder()
+        .appendSuper(super.equals(o))
+        .append(iteration, that.iteration)
+        .append(values, that.values)
+        .isEquals();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder(17, 37)
+        .appendSuper(super.hashCode())
+        .append(iteration)
+        .append(values)
+        .toHashCode();
   }
 }
