3e2608b8ea7d8818b90ff178fb7ee2ba681aa4c3
famaridon <famaridon@users.noreply.github.com>
2019-02-06 23:20:48 +0100

    unused import

--- redmine-client-api/src/main/java/com/famaridon/redminengapi/services/redmine/rest/client/beans/AbstractRedmineBean.java
+++ redmine-client-api/src/main/java/com/famaridon/redminengapi/services/redmine/rest/client/beans/AbstractRedmineBean.java
@@ -4,8 +4,6 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import java.io.Serializable;
-
 public abstract class AbstractRedmineBean {
 	
 	private long id;
