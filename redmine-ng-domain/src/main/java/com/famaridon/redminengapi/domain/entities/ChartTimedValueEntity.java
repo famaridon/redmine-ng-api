ca00b715ce2e852202202abbe6da3e523d9bdfa3
famaridon <famaridon@users.noreply.github.com>
2019-02-06 23:40:42 +0100

    Useless imports should be removed

--- redmine-ng-domain/src/main/java/com/famaridon/redminengapi/domain/entities/ChartTimedValueEntity.java
+++ redmine-ng-domain/src/main/java/com/famaridon/redminengapi/domain/entities/ChartTimedValueEntity.java
@@ -3,7 +3,6 @@ package com.famaridon.redminengapi.domain.entities;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import org.apache.commons.lang3.builder.EqualsBuilder;
@@ -11,9 +10,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @Entity
-@AttributeOverrides({
-    @AttributeOverride(name = "name", column = @Column(unique = false, nullable = true))
-})
+@AttributeOverride(name = "name", column = @Column(unique = false, nullable = true))
 public class ChartTimedValueEntity extends AbstractEntity {
 
   @Column(nullable = false)
