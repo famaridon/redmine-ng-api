27c3166d1097d4c7c53b8384f9caca1c26e66d5d
famaridon <famaridon@users.noreply.github.com>
2019-02-06 23:08:47 +0100

    Constants must no be in class use interfaces or enum

--- redmine-ng-web-rest-api/src/main/java/com/famaridon/redminengapi/rest/api/SecurityHeaders.java
+++ redmine-ng-web-rest-api/src/main/java/com/famaridon/redminengapi/rest/api/SecurityHeaders.java
@@ -1,5 +1,5 @@
 package com.famaridon.redminengapi.rest.api;
 
-public class SecurityHeaders {
-	public static final String X_REDMINE_API_KEY = "X-Redmine-API-Key";
+public interface SecurityHeaders {
+	String X_REDMINE_API_KEY = "X-Redmine-API-Key";
 }
