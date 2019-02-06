197f94e6a25d088e51e9517933985423c1172aa8
famaridon <famaridon@users.noreply.github.com>
2019-02-06 23:30:02 +0100

    "throws" declarations should not be superfluous

--- redmine-client/src/main/java/com/famaridon/redminengapi/services/redmine/rest/client/deserializer/CustomFieldDeserializer.java
+++ redmine-client/src/main/java/com/famaridon/redminengapi/services/redmine/rest/client/deserializer/CustomFieldDeserializer.java
@@ -4,7 +4,6 @@ import com.famaridon.redminengapi.services.redmine.rest.client.beans.ArrayCustom
 import com.famaridon.redminengapi.services.redmine.rest.client.beans.CustomField;
 import com.famaridon.redminengapi.services.redmine.rest.client.beans.StringCustomField;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
@@ -13,27 +12,27 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.io.IOException;
 
 public class CustomFieldDeserializer extends JsonDeserializer<CustomField> {
-	
-	@Override
-	public CustomField deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-		ObjectCodec oc = jsonParser.getCodec();
-		JsonNode node = oc.readTree(jsonParser);
-		JsonNode isMultiple = node.get("multiple");
-		CustomField customField;
-		if(isMultiple != null && isMultiple.asBoolean()) {
-			ArrayCustomField arrayCf = new ArrayCustomField();
-			customField = arrayCf;
-			JsonNode array = node.get("value");
-			for (int i = 0; i < array.size(); i++) {
-				arrayCf.getValue().add(array.get(i).asText());
-			}
-		} else {
-			StringCustomField stringCf= new StringCustomField();
-			customField = stringCf;
-			stringCf.setValue(node.get("value").asText());
-		}
-		customField.setId(node.get("id").asLong());
-		customField.setName(node.get("name").asText());
-		return customField;
-	}
+
+  @Override
+  public CustomField deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+    ObjectCodec oc = jsonParser.getCodec();
+    JsonNode node = oc.readTree(jsonParser);
+    JsonNode isMultiple = node.get("multiple");
+    CustomField customField;
+    if (isMultiple != null && isMultiple.asBoolean()) {
+      ArrayCustomField arrayCf = new ArrayCustomField();
+      customField = arrayCf;
+      JsonNode array = node.get("value");
+      for (int i = 0; i < array.size(); i++) {
+        arrayCf.getValue().add(array.get(i).asText());
+      }
+    } else {
+      StringCustomField stringCf = new StringCustomField();
+      customField = stringCf;
+      stringCf.setValue(node.get("value").asText());
+    }
+    customField.setId(node.get("id").asLong());
+    customField.setName(node.get("name").asText());
+    return customField;
+  }
 }
