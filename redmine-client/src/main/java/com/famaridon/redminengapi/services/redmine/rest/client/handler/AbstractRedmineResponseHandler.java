f2a2436a9b037bed63f3090dd8e04d6298717f59
famaridon <famaridon@users.noreply.github.com>
2019-03-19 14:09:09 +0100

    rework duplicated code

--- redmine-client/src/main/java/com/famaridon/redminengapi/services/redmine/rest/client/handler/AbstractRedmineResponseHandler.java
+++ redmine-client/src/main/java/com/famaridon/redminengapi/services/redmine/rest/client/handler/AbstractRedmineResponseHandler.java
@@ -1,8 +1,5 @@
 package com.famaridon.redminengapi.services.redmine.rest.client.handler;
 
-import com.famaridon.redminengapi.services.redmine.rest.client.module.RedmineClientModule;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
@@ -11,33 +8,29 @@ import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.entity.ContentType;
 
+import java.io.IOException;
+
 public abstract class AbstractRedmineResponseHandler<T> implements ResponseHandler<T> {
-
-  protected final ObjectMapper objectMapper;
-
-  protected AbstractRedmineResponseHandler() {
-    this.objectMapper = new ObjectMapper();
-    this.objectMapper.registerModule(new RedmineClientModule());
-  }
-
-  @Override
-  public T handleResponse(HttpResponse response) throws IOException {
-    StatusLine statusLine = response.getStatusLine();
-    HttpEntity entity = response.getEntity();
-    if (statusLine.getStatusCode() >= 300) {
-      throw new HttpResponseException(statusLine.getStatusCode(), statusLine.getReasonPhrase());
-    }
-    if (entity == null) {
-      throw new ClientProtocolException("Response contains no content");
-    }
-
-    ContentType contentType = ContentType.getOrDefault(entity);
-    if (!contentType.getMimeType().equals(ContentType.APPLICATION_JSON.getMimeType())) {
-      throw new ClientProtocolException("Unexpected content type:" + contentType);
-    }
-    return parse(entity);
-
-  }
-
-  protected abstract T parse(HttpEntity entity) throws IOException;
+	
+	@Override
+	public T handleResponse(HttpResponse response) throws IOException {
+		StatusLine statusLine = response.getStatusLine();
+		HttpEntity entity = response.getEntity();
+		if (statusLine.getStatusCode() >= 300) {
+			throw new HttpResponseException(statusLine.getStatusCode(), statusLine.getReasonPhrase());
+		}
+		if (entity == null) {
+			throw new ClientProtocolException("Response contains no content");
+		}
+		
+		ContentType contentType = ContentType.getOrDefault(entity);
+		if (!contentType.getMimeType().equals(getExpectedMimeType().getMimeType())) {
+			throw new ClientProtocolException("Unexpected content type:" + contentType);
+		}
+		return parse(entity, contentType);
+	}
+	
+	protected abstract ContentType getExpectedMimeType();
+	
+	protected abstract T parse(HttpEntity entity, ContentType contentType) throws IOException;
 }
